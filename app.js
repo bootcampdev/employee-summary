12b0b6fe86341c854d5b263db494b5bbe2232041
kihBootCamp <kimberleyheuer@hotmail.com>
2021-01-22 16:39:40 -0600

    i think it is all working! testing

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// inquirer recurive prompt option
+// inquirer-recurive npm module with prompt option
 inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 const recursive = require('inquirer-recursive');
-//const Employee = require("./lib/Employee");
 
 let employeeList = [];
 
@@ -24,6 +23,8 @@ let employeeList = [];
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+// the recursive type will repeat the prompts.
 
 inquirer.prompt([{
     type: 'recursive',
@@ -83,9 +84,7 @@ inquirer.prompt([{
         },
     ]
 }]).then(function (answers) {
-    console.log(answers.employees);
-
-
+  
     for (let emp in answers.employees) {
         //console.log(answers.employees[emp])
 
@@ -113,37 +112,18 @@ inquirer.prompt([{
                 break;
         }
 
-        console.log("New employee is: " + newEmployee.name);
-        console.log("New employee is: " + newEmployee.getRole());
+        // console.log("New employee is: " + newEmployee.name);
+        // console.log("New employee is: " + newEmployee.getRole());
         employeeList.push(newEmployee);
-        console.log("my employee list: " + employeeList)
+        //console.log("my employee list: " + employeeList)
     }
 
     const html = render(employeeList);
     //console.log(html);
 
-    fs.writeFile("myteam.html", html, (err) => {
+    fs.writeFile(outputPath, html, (err) => {
         if (err) { console.log(err); }
         else { console.log("Team file saved"); }
     })
 
 });
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
